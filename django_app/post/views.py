fa2ad430ea3e5b2e9b792d5ecc466e3bfc56441f
msjo91 <msjo91@gmail.com>
2017-04-07 19:31:16 +0900

    Register admin and create APIListView

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
 
 from post.models import Mood
 from post.serializers import MoodSerializer
@@ -13,7 +9,7 @@ class Mood_list(generics.ListAPIView):
     queryset = Mood.objects.all()
     serializer_class = MoodSerializer
 
+
 class Mood_detail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Mood.objects.all()
     serializer_class = MoodSerializer
-
