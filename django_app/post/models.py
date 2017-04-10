7c909e68351e2a082a39fd42f41bbed6bdd22b6a
msjo91 <msjo91@gmail.com>
2017-04-10 17:57:46 +0900

    Add author foreign key

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -9,12 +9,13 @@ class Mood(models.Model):
     MOOD_VERY_GOOD = 4
 
     MOOD_CHOICE = (
-        (MOOD_VERY_BAD, 'VeryBad'),
-        (MOOD_BAD, 'Bad'),
-        (MOOD_GOOD, 'Good'),
         (MOOD_VERY_GOOD, 'VeryGood'),
+        (MOOD_GOOD, 'Good'),
+        (MOOD_BAD, 'Bad'),
+        (MOOD_VERY_BAD, 'VeryBad'),
     )
 
+    author = models.ForeignKey('member.MyUser')
     created_date = models.DateTimeField(auto_now_add=True)
     mood_chk = models.IntegerField(choices=MOOD_CHOICE)
     mood_comment = models.CharField(max_length=400)
