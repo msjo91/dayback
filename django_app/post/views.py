bec4beeac369efe5c547e8812aa04b018db6b107
msjo91 <msjo91@gmail.com>
2017-04-10 17:58:55 +0900

    Change class name to PostSerializer and migrate

--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
 
 from post.models import Mood
-from post.serializers import MoodSerializer
+from post.serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Mood.objects.all()
-    serializer_class = MoodSerializer
+    serializer_class = PostSerializer
